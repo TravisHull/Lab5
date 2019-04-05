bdd5d9478397e0989d34452be5348fbcd54ef342
Travis Hull <travish@ku.edu>
2019-04-05 15:21:45 -0500

    Completed CreateUser html and php files

--- CreateUser.php
+++ CreateUser.php
@@ -1,24 +1,36 @@
 <?php
+echo '<div class="home-button">
+        <a href="https://people.eecs.ku.edu/~t819h638/Lab5/CreateUser.html">Back</a>
+      </div>';
 $u_id = $_POST["userID"];
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "t819h638", "di7toh3K", "t819h638");
 
 if ($mysqli->connect_errno)
 {
-  printf("Connect failed: %s\n", $mysqli->connect_error);
+  printf('Connect failed: %s\n', $mysqli->connect_error);;
   exit();
 }
 
-$query = "SELECT user_id FROM Users WHERE user_id = $u_id";
+$query = "SELECT user_id FROM Users WHERE user_id = '$u_id'";
 
-if ($result = $mysqli->query($query))
+if (mysqli_num_rows($mysqli->query($query)) === 1)
 {
-  echo "<script> alert('A user named $u_id already exists, please enter a different user name!');";
+  echo "<script> alert('A user named $u_id already exists, please enter a different user name!');</script>";
+  exit();
 }
 
+else if ($u_id === "")
+{
+  echo "<script> alert('Please enter a user name!');</script>";
+
+}
 else
 {
-  $query = "INSERT INTO Users (user_id) VALUES ('$u_id')";
+$query = "INSERT INTO Users (user_id) VALUES ('$u_id')";
+$mysqli->query($query);
+echo "<script> alert('entered else case');</script>";
 }
 
+$mysqli->close();
  ?>
